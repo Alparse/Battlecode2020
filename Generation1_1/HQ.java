077c5b9fa42cea436431c2174a06e25a41660b13
serha <serhat.alpar@outlook.com>
2020-01-18 13:48:40 -0600

    Testing

--- Generation1_1/HQ.java
+++ Generation1_1/HQ.java
@@ -58,7 +58,7 @@ public class HQ extends RobotPlayer {
                     break;
                 }
         }
-        if (miners_built >=3&&miners_built<minerTarget) {
+        if (miners_built >3&&miners_built<minerTarget) {
             for (Direction dir : directions)
                 if (Utility.tryBuild(RobotType.MINER, dir)) {
                     RobotInfo built_robot=rc.senseRobotAtLocation(myLoc.add(dir));
